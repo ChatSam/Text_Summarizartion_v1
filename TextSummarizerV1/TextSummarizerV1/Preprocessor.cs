c8b8a6465911d86860fdae0ae57ece33a1c8469c
samaraca <samaraca@mail.uc.edu>
2015-12-27 23:07:22 -0500

    Pre-processor partially completed
    - Implemented : toLowerCase , sentence segmentation and stopWord
    removal.
    - Upcoming : implement stemmer

---
+++ TextSummarizerV1/TextSummarizerV1/Preprocessor.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TextSummarizerV1
+{
+    class Preprocessor
+    {
+        //todo: check if the static keyword is needed
+        private static string _initialText;
+
+        public Preprocessor(string initialText)
+        {
+            _initialText = initialText;
+        }
+
+
+        /// <summary>
+        /// Manages the tasks of the preprocessor
+        /// </summary>
+        public void RunPreprocessor()
+        {
+            //convert to lower case (string text) 
+            string inTextLowerCase = _initialText.ToLower();
+
+            string stopWordFilePath = "../../Resources/Preprocessing/standard-stopwords.txt";
+
+            IList<string> inSenetences = SegmentSentences(inTextLowerCase);
+
+            IList<string> inTextNoStopWords = RemoveStopWords(ref inSenetences, stopWordFilePath);
+
+        }
+
+
+        private IList<string> SegmentSentences(string inTextLowerCase)
+        {
+            //todo: the function splits even sentences with commas - fix needed
+            var inSentences = Regex.Split(inTextLowerCase, @"(?<=[\.!\?])\s+");
+
+            return inSentences;
+        }
+
+
+        private IList<string> RemoveStopWords(ref IList<string> inSentences, string stopWordFilePath)
+        {
+
+            //initializing stop words
+            var stopWordListRaw = File.ReadAllText(stopWordFilePath);
+
+            var stopWordList = Regex.Split(stopWordListRaw, "\\W+");
+
+            //remove stop words
+            for (int i = 0; i < inSentences.Count; i++)
+            {
+                List<string> splitSentence = Regex.Split(inSentences[i], "\\W+").ToList();
+
+                splitSentence.RemoveAll(word => stopWordList.Contains(word));
+
+                inSentences[i] = string.Join("||", splitSentence);
+
+            }
+
+            return inSentences;
+        }
+
+
+       
+
+
+        //remove suffixes [tokenization and word stemmer]
+
+        //remove punctuation
+
+        //return 
+
+    }
+}
--- TextSummarizerV1/TextSummarizerV1/SummarizerMain.cs
+++ TextSummarizerV1/TextSummarizerV1/SummarizerMain.cs
@@ -1,15 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TextSummarizerV1
 {
-    class Program
+    class SummarizerMain
     {
         static void Main(string[] args)
         {
+
+            //get the document
+
+            string path = "..\\..\\Resources\\Test\\inputNews.txt";
+
+            //pass it to a string 
+            string initialText = File.ReadAllText(path);
+
+            //send it to preprocessing 
+            Preprocessor preprocessor = new Preprocessor(initialText);
+
+            preprocessor.RunPreprocessor();
+
+            //send it to feature extraction
+
+            //send it to sentence selection and eassembly
+
+            //send to generate summary
+
+            //evalute performance
+
+            //save summary to doc
+
+            //print summary 
+
         }
     }
 }
